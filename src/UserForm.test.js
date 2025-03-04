b8927c82e6c34e314dfbe9fd15d8292ee05f4e2e
BlackNBlue <niraj.bandal@appdirect.com>
2025-03-04 15:17:21 +0530

    added new test

--- src/UserForm.test.js
+++ src/UserForm.test.js
@@ -15,3 +15,32 @@ test("it shows two inputs and  a button", () => {
     expect(inputs).toHaveLength(2);
     expect(button).toBeInTheDocument();
 });
+
+ test("it calls onUserAdd when the form is submitted", () => {
+    //NOT good practice
+    const argList = [];
+    const callback = (...args) => argList.push(args);
+    //Render the component
+    render(<UserForm onUserAdd={callback}/>);
+
+    //Manipulate the component
+    const [nameInput, emailInput] = screen.getAllByRole('textbox');
+
+    //Simulate typing in name
+    user.click(nameInput);
+    user.keyboard('John Doe');
+
+    //Simulate typing in email
+    user.click(emailInput);
+    user.keyboard('jhon@doe.com');
+
+    //Find Button
+    const button = screen.getByRole('button');
+
+    //Simulate clicking the button
+    user.click(button);
+
+    //Assertion to make sure onUserAdd was called with name and email
+    expect(argList).toHaveLength(1);
+    expect(argList[0][0]).toEqual({name:'John Doe', email:'jhon@doe.com'});
+ });
