cf063d77216a37c6fd24d4a493c4f515d3870fbb
BlackNBlue <niraj.bandal@appdirect.com>
2025-03-12 10:28:00 +0530

    Added new test in UserList

--- src/UserList.test.js
+++ src/UserList.test.js
@@ -16,3 +16,21 @@ test('render one row per user', () => {
     //Assertion correct number of rows in table
     expect(rows).toHaveLength(2);
 });
+
+test('render email and name of each user', () =>{
+    const users = [
+        {name: 'jhon', email: 'jhon@jhon.com'},
+        {name: 'alex', email: 'alex@alex.com'},
+    ];
+    //Render component
+    render(<UserList users={users}/>);
+
+    //Check for every row in table
+    for (let user of users){
+        const name = screen.getByRole('cell', {name: user.name});
+        const email = screen.getByRole('cell', {name: user.email});
+
+        expect(name).toBeInTheDocument();
+        expect(email).toBeInTheDocument();
+    }
+});
