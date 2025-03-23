58d233c449c468ff61dea7fdd8c5b7d6fd24318a
BlackNBlue <niraj.bandal@appdirect.com>
2025-03-23 16:41:32 +0530

    written test file using RegExp()

--- sec7/src/components/repositories/RepositoriesSummary.test.js
+++ sec7/src/components/repositories/RepositoriesSummary.test.js
@@ -1,7 +1,7 @@
 import { screen, render } from '@testing-library/react';
 import RepositoriesSummary from './RepositoriesSummary';
 
-test('displays information about the repository', () => {
+ test('displays information about the repository', () => {
   const repository = {
     language: 'Javascript',
     stargazers_count: 5,
@@ -10,7 +10,7 @@ test('displays information about the repository', () => {
   };
   render(<RepositoriesSummary repository={repository} />);
 
-  //Shortcut to select many elements 
+  //Shortcut to select many elements
   for (let key in repository) {
     const value = repository[key];
     const element = screen.getByText(new RegExp(value));        //RegExp is used to match the value in the text
