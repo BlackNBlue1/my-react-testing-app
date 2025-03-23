be29e8a69372ba81e0ccec4cf3f9f41e2bdf6c73
BlackNBlue <niraj.bandal@appdirect.com>
2025-03-23 23:45:47 +0530

    written test case for new feature -> show icon with link for git repo

--- sec7/src/components/repositories/RepositoriesListItem.test.js
+++ sec7/src/components/repositories/RepositoriesListItem.test.js
@@ -11,15 +11,21 @@ function renderComponent() {
     name: 'react',
     html_url: 'https://github.com/facebook/react',
   };
+
   render(
     <MemoryRouter>
       <RepositoriesListItem repository={repository} />
     </MemoryRouter>
   );
+
+  return {repository};
 }
 
 test('shows a link to the github homepage for this repository', async() => {
-  renderComponent();
+  const {repository} = renderComponent();
 
   await screen.findByRole('img', {name: 'Javascript'});
+
+  const link = screen.getByRole('link', {name: 'github repository'});
+  expect(link).toHaveAttribute('href', repository.html_url);
 });
