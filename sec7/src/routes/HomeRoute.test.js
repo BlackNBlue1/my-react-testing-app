8f5a5ac30ec1c632dd3160b6c50c69a04ff26b85
BlackNBlue <niraj.bandal@appdirect.com>
2025-03-30 02:56:21 +0530

    created handler to make request

--- sec7/src/routes/HomeRoute.test.js
+++ sec7/src/routes/HomeRoute.test.js
@@ -4,3 +4,18 @@ import {MemoryRouter} from 'react-router-dom';
 import {rest} from 'msw';
 import {setupServer} from 'msw/node';
 
+const Handler = [
+    rest.get('api/repositories', (req, res,ctx) => {
+        const query = req.url.searchParams('q');
+        console.log(query);
+
+        return res(
+            ctx.json({
+                items: [
+                    {id:1, full_name:" first "},
+                    {id:2, full_name:" second "},
+                ]
+            })
+        )
+    })
+]
