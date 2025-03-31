a82518077dad77d1050649798be4cde1eee1f2ac
BlackNBlue <niraj.bandal@appdirect.com>
2025-03-31 23:41:05 +0530

    writing test and used memory route to render compornent

--- sec9/src/routes/HomeRoute.test.js
+++ sec9/src/routes/HomeRoute.test.js
@@ -1,30 +1,30 @@
 import {render, screen} from '@testing-library/react';
-import HomeRoute from './HomeRoute';
+import HomeRoute from '../../../sec9/src/routes/HomeRoute';
 import {MemoryRouter} from 'react-router-dom';
 import {rest} from 'msw';
 import {setupServer} from 'msw/node';
 
 const Handler = [
     rest.get('api/repositories', (req, res,ctx) => {
-        const query = req.url.searchParams('q');
+        const query = req.url.searchParams('q').split('language:')[1];  //Taking the language from the query
         console.log(query);
 
         return res(
             ctx.json({
                 items: [
-                    {id:1, full_name:" first "},
-                    {id:2, full_name:" second "},
-                ]
+                    {id:1, full_name:`${query}_first`},
+                    {id:2, full_name:`${query}_second`},
+                ],
             })
-        )
+        );
     })
 ]
 
 const server = setupServer(...Handler);
 
-beforeAll(() => server.listen());
-afterEach(() => server.resetHandlers());
-afterAll(() => server.close());
+beforeAll(() => {server.listen()});
+afterEach(() => {server.resetHandlers()});
+afterAll(() => {server.close()});
 
 test('return 2 links for each language', async () => {
     render(
@@ -33,5 +33,6 @@ test('return 2 links for each language', async () => {
         </MemoryRouter>
     );
 
+    screen.debug();
 
 });
