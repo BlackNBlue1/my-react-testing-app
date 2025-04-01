79bf51217d85f3d978e5ff8de3a9abd2857ee560
BlackNBlue <niraj.bandal@appdirect.com>
2025-04-01 15:22:21 +0530

    Grouping tests in describe

--- sec9/src/components/auth/AuthButton.test.js
+++ sec9/src/components/auth/AuthButton.test.js
@@ -1,12 +1,54 @@
 import { render, screen } from '@testing-library/react';
-import { MemoryRouter } from 'react-router-dom';
+import { MemoryRouter, redirect } from 'react-router-dom';
 import { createServer } from '../../test/server';
 import AuthButtons from './AuthButtons';
 
-test('when user is not signed in, sign in and sign up are visible', async () => {});
+const renderComponent = () =>{
+    render(
+        <MemoryRouter>
+            <AuthButtons />
+        </MemoryRouter>
+    );
+}
+describe('when user is not signed in ', () => {
+    createServer([
+        {
+            path: '/api/user',
+            res: (req, res, ctx) => {
+                return res(ctx.json({ user: null }));
+            },
+        },
+    ]);
+    test('when user is not signed in, sign in and sign up are visible', async () => {
+        renderComponent();
 
-test('when user is not signed in, sign out is not visible', async () => {});
+    });
 
-test('when user is signed in, sign in and sign up are not visible', async () => {});
+    test('when user is not signed in, sign out is not visible', async () => {
+        renderComponent();
 
-test('when user is signed in, sign out is visible', async () => {});
+    });
+
+})
+
+describe('when user is signed in', () => {
+    createServer([
+        {
+            path: '/api/user',
+            res: (req, res, ctx) => {
+                return res(ctx.json({ user: { id: 1, username: 'testuser' } }));
+            },
+        },
+    ]);
+    test('when user is signed in, sign in and sign up are not visible', async () => {
+        renderComponent();
+
+    });
+
+    test('when user is signed in, sign out is visible', async () => {
+        renderComponent();
+
+    });
+
+
+});
